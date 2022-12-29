a246a0cecd5e36afb55931502497618b77d0876a
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-12-29 13:36:18 +0100

    bootstrap

--- Marvel/src/app/app.module.ts
+++ Marvel/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
